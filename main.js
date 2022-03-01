66770d7300fd15c29d069a25b7c4b4a13be5fdcf
R3ahim <r9ahim@gmail.com>
2022-03-01 11:17:28 +0600

    almost complete

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ const displaySearchResult = collector =>{
     searchresult.textContent = '';
     // console.log(collector);
    collector.data.forEach(info =>{
-       console.log(info);
+    //    console.log(info);
+      const n =JSON.stringify(info.slug);
+    //   const b = JSON.parse(n)
+    //   console.log(n)
        const div = document.createElement('div');
        div.classList.add('col');
        div.innerHTML=`
@@ -31,7 +34,7 @@ const displaySearchResult = collector =>{
             <div class="card-body">
               <h3 class="card-title">${info.phone_name}</h3>
               <h4 class= "card-title"> ${info.brand}</h4>
-              <button  onclick="loadPhoneDeatails(${info.slug})" type="button" class="btn btn-secondary">click me</button>
+              <button onclick='loadPhoneDeatails(${n})' type="button" class="btn btn-secondary">click me</button>
 
             </div>
           </div>
@@ -44,11 +47,32 @@ const displaySearchResult = collector =>{
 
 // btn to search
 
-const loadPhoneDeatails =(phoneId) =>{
-   
- const url1 =`https://openapi.programming-hero.com/api/phone/${phoneId}`;
- fetch(url1)
- .then(res =>res.json())
- .then(data=>console.log(data))
+const loadPhoneDeatails = phoneId =>{
+    // console.log(phoneId)
 
-}
+    const url =`https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>loadDisplayDetails(data.data))
+     
+   
+   }
+
+
+   const loadDisplayDetails =(detals) =>{
+    //    console.log(detals.image,detals.phone_name,detals.brand);
+    console.log(detals);
+    const searchDetails = document.getElementById('search-details');
+    searchDetails.textContent =''
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML =  `
+    <img class="w-100" src="${detals.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h3 class="card-title">${detals.name}</h3>
+      <p class="card-text">${detals.releaseDate}</p>
+    </div>
+    ` ;
+    searchDetails.appendChild(div)
+
+   }
